49a97e411e1e87ddc240e2b55135be70f4350cda
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-08-04 12:44:46 +0200

    Added empty C++ Trie class

---
+++ setsync/Trie.cpp
@@ -0,0 +1,17 @@
+/*
+ * Trie.cpp
+ *
+ *  Created on: 24.07.2012
+ *      Author: till
+ */
+
+#include "Trie.h"
+
+Trie::Trie() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Trie::~Trie() {
+	// TODO Auto-generated destructor stub
+}
