63a2306329b73dfd85b4d54346942eed73ff894b
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-09-02 14:57:13 +0200

    Format changes and ignoring compiled unittests

--- setsync/tests/unittests/Main.cpp
+++ setsync/tests/unittests/Main.cpp
@@ -11,22 +11,21 @@
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 
-int main( int ac, char **av )
-{
-  //--- Create the event manager and test controller
-  CPPUNIT_NS::TestResult controller;
+int main(int ac, char **av) {
+	//--- Create the event manager and test controller
+	CPPUNIT_NS::TestResult controller;
 
-  //--- Add a listener that collects test result
-  CPPUNIT_NS::TestResultCollector result;
-  controller.addListener( &result );
+	//--- Add a listener that collects test result
+	CPPUNIT_NS::TestResultCollector result;
+	controller.addListener(&result);
 
-  //--- Add a listener that print dots as test run.
-  CPPUNIT_NS::BriefTestProgressListener progress;
-  controller.addListener( &progress );
+	//--- Add a listener that print dots as test run.
+	CPPUNIT_NS::BriefTestProgressListener progress;
+	controller.addListener(&progress);
 
-  //--- Add the top suite to the test runner
-  CPPUNIT_NS::TestRunner runner;
-  runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
-  runner.run( controller );
-  return result.wasSuccessful() ? 0 : 1;
+	//--- Add the top suite to the test runner
+	CPPUNIT_NS::TestRunner runner;
+	runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+	runner.run(controller);
+	return result.wasSuccessful() ? 0 : 1;
 }
