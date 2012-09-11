1cda419d0a5f3b04d09f04f7e46db0c067c5eacf
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-09-11 19:26:13 +0200

    Parallel execution of SHA1 test data calculation with openMP

--- setsync/tests/hash_generator/Main.cpp
+++ setsync/tests/hash_generator/Main.cpp
@@ -3,13 +3,17 @@
  *
  *      Author: Till Lorentzen
  */
-
+#include <omp.h>
+#include <iostream>
 #include "SHA1Generator.h"
 
+
 int main(int ac, char **av) {
-	uint64_t stepsize = 100000000;
-	for (int i = 0; i < 2; i++) {
+	uint64_t stepsize = 1000000;
+#pragma omp parallel for
+	for (int i = 0; i < 1000; i++) {
 		SHA1Generator generator(i * stepsize, i * stepsize + stepsize);
 		generator.run();
+//		std::cout << "Thread number: " << omp_get_thread_num() << std::endl;
 	}
 }
