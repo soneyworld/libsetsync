a6fc78dbb24156bf4bfd56bbb0dddc03af18847e
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-10-11 22:09:41 +0200

    Added a complete new idea of a DB node
    DbNode class is new and should be much more easier to use

--- setsync/trie/DBTrie.cpp
+++ setsync/trie/DBTrie.cpp
@@ -5,9 +5,9 @@
  */
 
 #include "DBTrie.h"
+#include "setsync/utils/bitset.h"
 #include <stdexcept>
 #include <typeinfo>
-
 
 #define ROOT_NAME "root"
 
@@ -19,6 +19,188 @@ DBValue::DBValue() {
 	memset(this->prefix, 0x0, MAXFIELD);
 	memset(this->parent, 0x0, MAXFIELD);
 	this->prefix_mask = 0;
+}
+
+unsigned char DbNode::hashscratch[MAXFIELD * 2];
+unsigned char DbNode::nullarray[MAXFIELD];
+char DbNode::root_name[] = "root";
+
+DbNode::DbNode(Db * db) :
+	db_(db), hasParent_(false) {
+	memset(nullarray,0x0,MAXFIELD);
+	DbInnerNode root_node;
+	Dbt key(root_name, strlen(root_name));
+	Dbt data(&root_node, sizeof(DbInnerNode));
+	int ret = this->db_->get(NULL, &key, &data, 0);
+	if (ret == 0) {
+		memcpy(this->hash,root_node.hash, MAXFIELD);
+		memset(this->parent,0x0, MAXFIELD);
+		memcpy(this->smaller, root_node.value.smaller,MAXFIELD);
+		memcpy(this->larger, root_node.value.larger,MAXFIELD);
+		int n = memcmp(this->smaller, nullarray,MAXFIELD);
+		if(n==0){
+			this->hasChildren_ = false;
+		}
+	} else if(ret == DB_NOTFOUND) {
+
+	}
+}
+
+DbNode::DbNode(Db * db, const unsigned char * hash) :
+	db_(db) {
+	DBValue result;
+	memcpy(this->hash, hash, MAXFIELD);
+	Dbt key(this->hash, MAXFIELD);
+	Dbt data(&result, sizeof(DBValue));
+	int ret = this->db_->get(NULL, &key, &data, 0);
+	if (ret == 0) {
+		memcpy(this->parent,result.parent,MAXFIELD);
+		memcpy(this->smaller,result.smaller,MAXFIELD);
+		memcpy(this->larger,result.larger,MAXFIELD);
+	} else {
+		throw "Node nicht gefunden";
+	}
+}
+
+DbNode DbNode::getNode(const unsigned char * hash){
+
+}
+
+bool DbNode::similar(const DbNode& node) const {
+	for (uint8_t i = 0; i < this->prefix_mask; i++) {
+		if (BITTEST(this->prefix, i) != BITTEST(this->prefix,i)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool DbNode::insert(DbNode& node) {
+	return insert(node, true);
+}
+
+bool DbNode::insert(const unsigned char * hash) {
+	return insert(hash, true);
+}
+
+bool DbNode::erase(DbNode& node, bool performHash) {
+	//TODO
+	return false;
+}
+
+bool DbNode::insert(DbNode& node, bool performHash) {
+	//TODO
+	return false;
+}
+
+bool DbNode::erase(DbNode& node) {
+	return erase(node, true);
+}
+
+bool DbNode::erase(const unsigned char * hash) {
+	return erase(hash, true);
+}
+
+bool DbNode::operator ==(const DbNode& other) const {
+	if (this->prefix_mask != other.prefix_mask)
+		return false;
+	if (this->dirty_ != other.dirty_)
+		return false;
+	if (this->hasChildren_ != other.hasChildren_)
+		return false;
+	if (this->hasParent_ != other.hasParent_)
+		return false;
+	int n = memcmp(this->hash, other.hash, MAXFIELD);
+	if (n != 0)
+		return false;
+	if (this->hasParent_) {
+		n = memcmp(this->parent, other.parent, MAXFIELD);
+		if (n != 0)
+			return false;
+	}
+	if (this->dirty_) {
+		n = memcmp(this->smaller, other.smaller, MAXFIELD);
+		if (n != 0)
+			return false;
+		n = memcmp(this->larger, other.larger, MAXFIELD);
+		if (n != 0)
+			return false;
+	}
+	return true;
+}
+
+bool DbNode::operator !=(const DbNode& other) const {
+	return !this->operator ==(other);
+}
+
+bool DbNode::operator <(const DbNode& other) const {
+	return BITTEST( (unsigned char *)(other.prefix) ,this->prefix_mask);
+}
+
+bool DbNode::operator >(const DbNode& other) const {
+	return BITTEST( (unsigned char *)(other.prefix) ,this->prefix_mask);
+}
+
+DbNode::DbNode(const DbNode& other) :
+	dirty_(other.dirty_), hasChildren_(other.hasChildren_),
+			hasParent_(other.hasParent_), prefix_mask(other.prefix_mask),
+			db_(other.db_) {
+	memcpy(this->hash, other.hash, MAXFIELD);
+	memcpy(this->parent, other.parent, MAXFIELD);
+	memcpy(this->prefix, other.prefix, MAXFIELD);
+	memcpy(this->smaller, other.smaller, MAXFIELD);
+	memcpy(this->larger, other.larger, MAXFIELD);
+}
+
+DbNode& DbNode::operator=(const DbNode& rhs) {
+	this->db_ = rhs.db_;
+	this->dirty_ = rhs.dirty_;
+	this->hasChildren_ = rhs.hasChildren_;
+	this->hasParent_ = rhs.hasParent_;
+	this->prefix_mask = rhs.prefix_mask;
+	memcpy(this->hash, rhs.hash, MAXFIELD);
+	memcpy(this->parent, rhs.parent, MAXFIELD);
+	memcpy(this->prefix, rhs.prefix, MAXFIELD);
+	memcpy(this->smaller, rhs.smaller, MAXFIELD);
+	memcpy(this->larger, rhs.larger, MAXFIELD);
+	return *this;
+}
+
+DbNode DbNode::getSmaller() {
+	if (hasChildren_) {
+		DbNode result = DbNode(this->db_, this->smaller);
+		return result;
+	}
+}
+DbNode DbNode::getLarger() {
+	if (hasChildren_) {
+		DbNode result = DbNode(this->db_, this->larger);
+		return result;
+	}
+}
+DbNode DbNode::getParent() {
+	if (hasParent_) {
+		DbNode result = DbNode(this->db_, this->parent);
+		return result;
+	}
+}
+
+bool DbNode::insert(const unsigned char * hash, bool performHash) {
+	//TODO
+	return false;
+}
+
+bool DbNode::erase(const unsigned char * hash, bool performHash) {
+	//TODO
+	return false;
+}
+
+void DbNode::updateHash(void) {
+	if (hasChildren_) {
+		memcpy(DbNode::hashscratch, this->smaller, MAXFIELD);
+		memcpy(DbNode::hashscratch + MAXFIELD, this->larger, MAXFIELD);
+		SHA1(DbNode::hashscratch, 2 * MAXFIELD, this->hash);
+	}
 }
 
 DBTrie::DBTrie(Db * db, const size_t hashsize) :
@@ -35,13 +217,66 @@ DBTrie::~DBTrie() {
 	}
 }
 
-bool DBTrie::add(const unsigned char * hash, bool performhash) {
+bool DBTrie::insertHash(const unsigned char * hash) {
 	if (this->root_ == NULL) {
 		this->root_ = new DbInnerNode;
 		memcpy(this->root_->hash, hash, this->getHashSize());
 		memcpy(this->root_->value.prefix, hash, this->getHashSize());
 		this->root_->value.prefix_mask = 8 * this->getHashSize();
+		putRootToDB();
+		return true;
+	} else {
+		DbInnerNode newnode;
+		memcpy(newnode.hash, hash, this->getHashSize());
+		memcpy(newnode.value.prefix, hash, this->getHashSize());
+		newnode.value.prefix_mask = 8 * this->getHashSize();
+		return insertAtNode(hash, this->root_, &newnode);
 	}
+}
+
+bool DBTrie::insertAtNode(const unsigned char * hash,
+		DbInnerNode * currentnode, DbInnerNode * newnode) {
+	bool thesame = similar(newnode->value.prefix, currentnode->value.prefix,
+			currentnode->value.prefix_mask);
+	if (thesame) {
+		DbInnerNode next;
+		bool
+				larger =
+						BITTEST( (unsigned char *)(newnode->value.prefix) ,currentnode->value.prefix_mask);
+		if (larger)
+			if (!loadNode(&next, currentnode->value.larger))
+				throw "";
+			else if (!loadNode(&next, currentnode->value.smaller))
+				throw "";
+
+	}
+	return false;
+}
+
+bool DBTrie::similar(unsigned char *a, unsigned char *b, uint8_t count) {
+	for (uint8_t i = 0; i < count; i++) {
+		if (BITTEST(a, i) != BITTEST(b,i)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool DBTrie::loadNode(DbInnerNode * target, const unsigned char * hash) {
+	unsigned char sha[this->getHashSize()];
+	memcpy(sha, hash, this->getHashSize());
+	memcpy(target->hash, hash, this->getHashSize());
+	Dbt key(sha, this->getHashSize());
+	Dbt data(&target->value, sizeof(DBValue));
+	int ret = this->db_->get(NULL, &key, &data, 0);
+	if (ret == DB_NOTFOUND) {
+		return false;
+	} else if (ret == 0) {
+		return true;
+	}
+}
+
+bool DBTrie::add(const unsigned char * hash, bool performhash) {
 	unsigned char sha[this->getHashSize()];
 	memcpy(sha, hash, this->getHashSize());
 	Dbt key(sha, this->getHashSize());
@@ -50,8 +285,12 @@ bool DBTrie::add(const unsigned char * hash, bool performhash) {
 	if (ret == DB_KEYEXIST) {
 		return false;
 	} else if (ret == 0) {
-		this->incSize();
-		return true;
+		if (insertHash(hash)) {
+			this->incSize();
+			return true;
+		} else {
+			return false;
+		}
 	}
 	return false;
 }
@@ -66,7 +305,7 @@ bool DBTrie::remove(const unsigned char * hash, bool performhash) {
 		return false;
 	} else if (ret == 0) {
 		this->decSize();
-		if(this->getSize()==0){
+		if (this->getSize() == 0) {
 			this->clear();
 		}
 		return true;
@@ -128,8 +367,7 @@ const DBTYPE DBTrie::getTableType() {
 
 bool DBTrie::operator ==(const Trie& other) const {
 	try {
-		const DBTrie& other_ =
-				dynamic_cast<const DBTrie&> (other);
+		const DBTrie& other_ = dynamic_cast<const DBTrie&> (other);
 		if (this->root_ != other_.root_)
 			return false;
 		if (this->root_ == NULL)
--- setsync/trie/DBTrie.h
+++ setsync/trie/DBTrie.h
@@ -32,6 +32,52 @@ public:
 	unsigned char hash[MAXFIELD];
 };
 
+class DbNode {
+private:
+	static unsigned char hashscratch[MAXFIELD * 2];
+	static unsigned char nullarray[MAXFIELD];
+	static char root_name[];
+	Db * db_;
+	bool similar(const DbNode& node) const;
+	uint8_t commonPrefixSize(DbNode& other) const;
+	bool hasParent_;
+	bool hasChildren_;
+	bool dirty_;
+	unsigned char hash[MAXFIELD];
+	unsigned char parent[MAXFIELD];
+	unsigned char smaller[MAXFIELD];
+	unsigned char larger[MAXFIELD];
+	unsigned char prefix[MAXFIELD];
+	uint8_t prefix_mask;
+	DbNode(Db * db, const unsigned char * hash);
+	void setParent(DbNode& parent);
+	void setSmaller(DbNode& smaller);
+	void setLarger(DbNode& larger);
+	bool insert(DbNode& node);
+	bool insert(DbNode& node, bool performHash);
+	bool erase(DbNode& node);
+	bool erase(DbNode& node, bool performHash);
+	void updateHash();
+	DbNode getNode(const unsigned char * hash);
+public:
+	DbNode(Db * db);
+	DbNode(const DbNode& other);
+	bool hasChildren();
+	bool hasParent();
+	DbNode getSmaller();
+	DbNode getLarger();
+	DbNode getParent();
+	bool insert(const unsigned char * hash);
+	bool insert(const unsigned char * hash, bool performHash);
+	bool erase(const unsigned char * hash);
+	bool erase(const unsigned char * hash, bool performHash);
+	bool operator ==(const DbNode& other) const;
+	bool operator !=(const DbNode& other) const;
+	bool operator >(const DbNode& other) const;
+	bool operator <(const DbNode& other) const;
+	DbNode& operator=( const DbNode& rhs );
+};
+
 class DBTrie: public trie::Trie,
 		public virtual berkeley::BerkeleyDBTableUserInferface {
 private:
@@ -39,6 +85,11 @@ private:
 	DbInnerNode * root_;
 	void getRootFromDB();
 	void putRootToDB();
+	bool insertHash(const unsigned char * hash);
+	bool insertAtNode(const unsigned char * hash, DbInnerNode * currentnode,
+			DbInnerNode * newnode);
+	bool similar(unsigned char *a, unsigned char *b, uint8_t count);
+	bool loadNode(DbInnerNode * target, const unsigned char * hash);
 public:
 	DBTrie(Db * db, const size_t hashsize = SHA_DIGEST_LENGTH);
 	virtual ~DBTrie();
