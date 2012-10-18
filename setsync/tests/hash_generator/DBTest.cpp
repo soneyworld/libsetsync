8f55865a5f74b28b1eb05f2ef5f169a0dd60b03d
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-10-18 14:26:48 +0200

    Added a little performance test for BDB
    Inserts gets and deletes entries, once on memory,
    once on file system

--- setsync/tests/hash_generator/DBTest.cpp
+++ setsync/tests/hash_generator/DBTest.cpp
@@ -6,15 +6,93 @@
 
 #include "DBTest.h"
 
+using namespace std;
 DBTest::DBTest() {
-	// TODO Auto-generated constructor stub
 
 }
 
 DBTest::~DBTest() {
-	// TODO Auto-generated destructor stub
 }
 
-void DBTest::run(){
+void DBTest::run() {
+	runMemDb();
+	runFsDb();
+}
 
+void DBTest::put(Db * db, pair<Dbt, Dbt> values) {
+	db->put(NULL, &(values.first), &(values.second), 0);
+}
+void DBTest::get(Db * db, Dbt key) {
+	Dbt value;
+	db->get(NULL, &key, &value, 0);
+
+}
+void DBTest::del(Db * db, Dbt key) {
+	db->del(NULL, &key, 0);
+}
+
+void DBTest::runMemDb() {
+	cout << "running Berkeley DB (mem) test:" << endl;
+	Db db(NULL, 0);
+	db.open(NULL, NULL, NULL, DB_BTREE, DB_CREATE, 0);
+	clock_t start, stop, duration;
+	duration = 0;
+	for (int iter = 0; iter < LOOP_ITERATIONS; iter++) {
+		start = clock();
+		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
+			Dbt key(&i, sizeof(int));
+			Dbt value(NULL, 0);
+			pair<Dbt, Dbt> p(key, value);
+			put(&db, p);
+		}
+		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
+			Dbt key(&i, sizeof(int));
+			get(&db, key);
+		}
+		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
+			Dbt key(&i, sizeof(int));
+			del(&db, key);
+		}
+		stop = clock();
+		duration += stop - start;
+	}
+	cout << ITERATIONS << " adds/gets/inserts after " << (float) duration
+			/ CLOCKS_PER_SEC;
+	cout << " sec (~" << (long) ((long) ITERATIONS / (float) (duration
+			/ CLOCKS_PER_SEC)) << " per sec)" << endl;
+	db.close(0);
+}
+
+void DBTest::runFsDb() {
+	cout << "running Berkeley DB (fs) test:" << endl;
+	Db db(NULL, 0);
+	db.open(NULL, "temp-table.db", "test" , DB_BTREE, DB_CREATE, 0);
+	clock_t start, stop, duration;
+	duration = 0;
+	for (int iter = 0; iter < LOOP_ITERATIONS; iter++) {
+		start = clock();
+		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
+			Dbt key(&i, sizeof(int));
+			Dbt value(NULL, 0);
+			pair<Dbt, Dbt> p(key, value);
+			put(&db, p);
+		}
+		db.sync(0);
+		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
+			Dbt key(&i, sizeof(int));
+			get(&db, key);
+		}
+		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
+			Dbt key(&i, sizeof(int));
+			del(&db, key);
+		}
+		stop = clock();
+		duration += stop - start;
+	}
+	cout << ITERATIONS << " adds/gets/inserts after " << (float) duration
+			/ CLOCKS_PER_SEC;
+	cout << " sec (~" << (long) ((long) ITERATIONS / (float) (duration
+			/ CLOCKS_PER_SEC)) << " per sec)" << endl;
+	db.close(0);
+	remove("temp-table.db");
 }
--- setsync/tests/hash_generator/DBTest.h
+++ setsync/tests/hash_generator/DBTest.h
@@ -7,8 +7,16 @@
 #ifndef DBTEST_H_
 #define DBTEST_H_
 #include "Test.h"
+#include <utility>
+#include <db_cxx.h>
 
-class DBTest :public Test{
+class DBTest: public Test {
+private:
+	void runMemDb();
+	void runFsDb();
+	void put(Db * db, std::pair<Dbt, Dbt> values);
+	void get(Db * db, Dbt key);
+	void del(Db * db, Dbt key);
 public:
 	DBTest();
 	void run();
