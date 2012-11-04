8f89b2119a0a15122b0bf29fac06ffb763c58d06
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-11-04 13:32:49 +0100

    Documentation added

--- setsync/utils/CryptoHash.h
+++ setsync/utils/CryptoHash.h
@@ -1,6 +1,6 @@
 /*
  *
- *      Author: Till Lorentzen
+ * @author	Till Lorentzen
  */
 
 #ifndef CRYPTOHASH_H_
@@ -13,26 +13,46 @@
 #else
 #include "md.h"
 #endif
-namespace utils{
-
-class CryptoHash{
+namespace utils {
+/**
+ * Wrapper class to provide a generic interface for polarssl and openssl
+ */
+class CryptoHash {
 private:
 #ifdef HAVE_OPENSSL
+	/// Hash algorithm properties of OPENSSL
 	const EVP_MD *digit;
 #else
+	/// Hash algorithm properties of polarssl
 	const md_info_t *digit;
 #endif
 	int hash(unsigned char * target_md, const char * str) const;
-	int hash(unsigned char * target_md, const unsigned char * data, const std::size_t length) const;
+	int hash(unsigned char * target_md, const unsigned char * data,
+			const std::size_t length) const;
 	int hash(unsigned char * target_md, const std::string& str) const;
 	int hash(unsigned char * target_md, std::istream& in) const;
 public:
+	/**
+	 * Possible hash algorithms are: md2, md4, md5, sha1, sha224, sha256, sha384, sha512
+	 *
+	 * \param name of the hash algorithm
+	 */
 	CryptoHash(const char * name = "md5");
+	/**
+	 * default destructor
+	 */
 	virtual ~CryptoHash();
+	/**
+	 * \return memory size of the hash digit in bytes
+	 */
 	const std::size_t getHashSize() const;
+	/**
+	 * \return 20
+	 */
 	static std::size_t getDefaultDigitLength();
 	int operator()(unsigned char * target_md, const char * str) const;
-	int operator()(unsigned char * target_md, const unsigned char * data, const std::size_t length) const;
+	int operator()(unsigned char * target_md, const unsigned char * data,
+			const std::size_t length) const;
 	int operator()(unsigned char * target_md, const std::string& str) const;
 	int operator()(unsigned char * target_md, std::istream& in) const;
 };
