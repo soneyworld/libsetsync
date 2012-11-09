a10e7c8b7951723aef7f0d0fa16c1e998c75e4c2
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-11-09 17:40:56 +0100

    Added --help paramter for evaluation binary

--- setsync/tests/evaluation/Main.cpp
+++ setsync/tests/evaluation/Main.cpp
@@ -20,19 +20,34 @@ int main(int ac, char **av) {
 		std::string arg(av[i]);
 		args.insert(arg);
 	}
+	if (!(args.find(std::string("--help"))== args.end())){
+		std::cout << "Possible parameter tests are: "<< std::endl;
+		std::cout << "--bf-test"<< std::endl;
+		std::cout << "--bdb-test"<< std::endl;
+		std::cout << "--sql-test"<< std::endl;
+		std::cout << "--all-tests"<< std::endl;
+		return 0;
+	}
+	bool bdb = !(args.find(std::string("--bdb-test"))==args.end());
+	bool bf = !(args.find(std::string("--bf-test"))==args.end());
+	bool all = !(args.find(std::string("--all-tests"))==args.end());
+#ifdef HAVE_SQLITE
+	bool sqlite = !(args.find(std::string("--sql-test"))==args.end());
+#endif
 
-	if (!(args.find(std::string("--bf-test"))==args.end())) {
+
+	if (bf || all) {
 		BFTest bftest;
 		bftest.run();
 	}
 
-	if (!(args.find(std::string("--bdb-test"))==args.end())) {
+	if (bdb || all) {
 		DBTest dbtest;
 		dbtest.run();
 	}
 
 #ifdef HAVE_SQLITE
-	if (!(args.find(std::string("--sql-test"))==args.end())) {
+	if (sqlite || all) {
 		SQLiteTest sqltest;
 		sqltest.run();
 	}
