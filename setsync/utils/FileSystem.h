57da5270517c5ce6743e912a535baa35d0662918
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-11-12 10:53:20 +0100

    Added documentation

--- setsync/utils/FileSystem.h
+++ setsync/utils/FileSystem.h
@@ -8,10 +8,22 @@
 #define FILESYSTEM_H_
 #include <inttypes.h>
 namespace utils {
-
+/**
+ * Utilities for file system operations
+ */
 class FileSystem {
 public:
+	/**
+	 * Removes a complete directory recursively
+	 * \param path to the directory, which should be deleted
+	 * \return 0 on success
+	 */
 	static int rmDirRecursive(const char *path);
+	/**
+	 * Returns the size of a given file
+	 * \param file which size should be returned
+	 * \return size of the given file
+	 */
 	static uint64_t fileSize(const char *file);
 };
 
