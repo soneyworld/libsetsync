3eadf22bcfaa163cedb2cc456316dd73d051df75
Till Lorentzen <t.lorentzen@tu-bs.de>
2013-01-11 19:29:20 +0100

    Documentation added

--- setsync/crypto/CryptoHash.h
+++ setsync/crypto/CryptoHash.h
@@ -16,6 +16,9 @@
 #include "md.h"
 #endif
 namespace setsync {
+/**
+ * All cryptographic hash functions are at this namespace
+ */
 namespace crypto {
 /**
  * Wrapper class to provide a generic interface for polarssl and openssl
@@ -31,11 +34,50 @@ private:
 	/// Hash algorithm properties of polarssl
 	const md_info_t *digit;
 #endif
+	/**
+	 * Hashes the given input string into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param str to be hashed into the target_md
+	 * \return true on success and false on fail
+	 */
 	int hash(unsigned char * target_md, const char * str) const;
+	/**
+	 * Hashes the given data with the given length into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param data to be hashed into the target_md
+	 * \param length of the data array
+	 * \return true on success and false on fail
+	 */
 	int hash(unsigned char * target_md, const unsigned char * data,
 			const std::size_t length) const;
+	/**
+	 * Hashes the given input string into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param str to be hashed into the target_md
+	 * \return true on success and false on fail
+	 */
 	int hash(unsigned char * target_md, const std::string& str) const;
+	/**
+	 * Hashes the given input stream into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param in input stream to be hashed into the target_md
+	 * \return true on success and false on fail
+	 */
 	int hash(unsigned char * target_md, std::istream& in) const;
+	/**
+	 * Initializes the needed data structures for later usage
+	 *
+	 * It will only be called by the constructor
+	 *
+	 */
 	void init(void);
 public:
 	/**
@@ -60,7 +102,11 @@ public:
 	 * \param name of the hash algorithm
 	 */
 	CryptoHash(const std::string& name);
-
+	/**
+	 * copy constructor
+	 *
+	 * \param copy the instance which should be copied
+	 */
 	CryptoHash(const CryptoHash& copy);
 	/**
 	 * default destructor
@@ -70,32 +116,115 @@ public:
 	 * \return memory size of the hash digit in bytes
 	 */
 	std::size_t getHashSize() const;
-
+	/**
+	 * Hashes the given input string into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param str to be hashed into the target_md
+	 * \return true on success and false on fail
+	 */
 	int operator()(unsigned char * target_md, const char * str) const;
+	/**
+	 * Hashes the given data with the given length into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param data to be hashed into the target_md
+	 * \param length of the data array
+	 * \return true on success and false on fail
+	 */
 	int operator()(unsigned char * target_md, const unsigned char * data,
 			const std::size_t length) const;
+	/**
+	 * Hashes the given input stream into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param in input stream to be hashed into the target_md
+	 * \return true on success and false on fail
+	 */
 	int operator()(unsigned char * target_md, const std::string& str) const;
+	/**
+	 * Hashes the given input stream into the given target_md
+	 * buffer
+	 *
+	 * \param target_md pointer to an array to save the finished hash into
+	 * \param in input stream to be hashed into the target_md
+	 * \return true on success and false on fail
+	 */
 	int operator()(unsigned char * target_md, std::istream& in) const;
 	/**
 	 * \return the name of the chosen crypto hash algorithm
 	 */
 	std::string getName(void) const;
 };
-
+/**
+ *
+ */
 class CryptoHashContainer {
 private:
+	/// pointer to the allocated array where the Message Digit is stored
 	unsigned char * md_;
+	/// crypto hash function which has been used to create the Message Digit
 	const CryptoHash& hashFunction_;
 public:
+	/**
+	 * Creates
+	 *
+	 * \param hash function used for the data of the container
+	 * \param md to be saved into the container
+	 */
 			CryptoHashContainer(const CryptoHash& hash,
 					const unsigned char * md = NULL);
-	virtual ~CryptoHashContainer();
-	void set(const unsigned char * md);
-	const unsigned char * get(void) const;
+	/**
+	 * Copies the given container values into a new instance
+	 *
+	 * \param other Container, which should be copied
+	 */
 	CryptoHashContainer(const CryptoHashContainer& other);
+	/**
+	 * destructor, which cleans the allocated space at md_
+	 */
+	virtual ~CryptoHashContainer();
+	/**
+	 * \param md to be saved into the Container
+	 */
+	void set(const unsigned char * md);
+	/**
+	 * Returns a const pointer to the saved Message Digit of this
+	 * Container
+	 *
+	 * \return a pointer to the saved message digit
+	 */
+	const unsigned char * get(void) const;
+	/**
+	 * Compares the given Container hash functions and its message digits
+	 *
+	 * \param other container, which should be compared
+	 * \returns true if both are equal, otherwise false
+	 */
 	bool operator ==(const CryptoHashContainer& other) const;
+	/**
+	 * Compares the given Container hash functions and its message digits
+	 *
+	 * \param other container, which should be compared
+	 * \returns true if both are different, otherwise false
+	 */
 	bool operator !=(const CryptoHashContainer& other) const;
+	/**
+	 * Compares the given message digits with the internal message
+	 *
+	 * \param other message digit, which should be compared
+	 * \returns true if both are equal, otherwise false
+	 */
 	bool operator ==(const unsigned char * other) const;
+	/**
+	 * Compares the given message digits with the internal message
+	 *
+	 * \param other message digit, which should be compared
+	 * \returns true if both are different, otherwise false
+	 */
 	bool operator !=(const unsigned char * other) const;
 };
 
