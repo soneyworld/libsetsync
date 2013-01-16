e0299678e9aaefebad2519682de93f029a19dbe0
Till Lorentzen <t.lorentzen@tu-bs.de>
2013-01-16 22:52:26 +0100

    Evaluation Scripts added

---
+++ setsync/tests/evaluation/eval-bf-p-10000.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+_PATH="eval-bf-p-10000"
+mkdir $_PATH
+
+MIN_P=.0001
+MAX_P=.1
+A=10000
+B=0
+
+MINSALT=0
+MAXSALT=10
+
+SALT=$MINSALT
+
+
+float_scale=8
+
+#####################################################################
+# Evaluate a floating point number expression.
+
+function float_eval()
+{
+    local stat=0
+    local result=0.0
+    if [[ $# -gt 0 ]]; then
+        result=$(echo "scale=$float_scale; $*" | bc -q 2>/dev/null)
+        stat=$?
+        if [[ $stat -eq 0  &&  -z "$result" ]]; then stat=1; fi
+    fi
+    echo $result
+    return $stat
+}
+
+#####################################################################
+# Evaluate a floating point number conditional expression.
+
+function float_cond()
+{
+    local cond=0
+    if [[ $# -gt 0 ]]; then
+        cond=$(echo "$*" | bc -q 2>/dev/null)
+        if [[ -z "$cond" ]]; then cond=0; fi
+        if [[ "$cond" != 0  &&  "$cond" != 1 ]]; then cond=0; fi
+    fi
+    local stat=$((cond == 0))
+    return $stat
+}
+
+
+## 10 Rounds
+while [ "$SALT" != "$MAXSALT" ]; do
+	COUNTER=$MIN_P
+	while  float_cond "$COUNTER <= $MAX_P"; do
+		FILE="${_PATH}/a_${A}b_${B}equal_${B}p_${COUNTER}_BF_salt_${SALT}.csv"
+		./ssync -s memDB A $A B $B -e $B --salt $SALT --type BF --bf $COUNTER > $FILE
+		echo "$FILE"
+		COUNTER=$(float_eval "$COUNTER + $MIN_P")
+	done
+	let SALT=SALT+1
+done
---
+++ setsync/tests/evaluation/eval-trie-traffic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+_PATH="eval-trie-traffic"
+mkdir $_PATH
+
+MIN_A=500
+MAX_A=1000
+B=1000
+
+MINSALT=0
+MAXSALT=10
+
+SALT=$MINSALT
+
+## 10 Rounds
+while [ "$SALT" != "$MAXSALT" ]; do
+	COUNTER=$MIN_A
+	while [ "$COUNTER" != "$MAX_A" ]; do
+		FILE="${_PATH}/a_${COUNTER}b_${B}equal_${COUNTER}_TRIE_salt_${SALT}.csv"
+		./ssync -s memDB A $COUNTER B $B -e $COUNTER --salt $SALT --type TRIE > $FILE
+		echo "$FILE"
+		let COUNTER=COUNTER+1
+	done
+	let SALT=SALT+1
+done
