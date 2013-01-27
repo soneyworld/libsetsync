13e6480e5b472bbfc790aced60b4b81ae6fffada
Till Lorentzen <t.lorentzen@tu-bs.de>
2013-01-27 19:29:46 +0100

    Added phase duration to measurements

--- setsync/tests/evaluation/SpeedTest.cpp
+++ setsync/tests/evaluation/SpeedTest.cpp
@@ -20,23 +20,27 @@ SpeedTest::~SpeedTest() {
 }
 
 void SpeedTest::run() {
-	cout << "operation,number,duration"<< endl;
+	cout << "operation,number,duration,phaseduration" << endl;
 	unsigned char buffer[hash.getHashSize()];
 	unsigned char value[200];
 	StopWatch watch;
+	StopWatch duration;
 	for (int iter = 0; iter < LOOP_ITERATIONS; iter++) {
 		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
 			stringstream ss;
 			ss << "test" << iter * ITEMS_PER_LOOPS + i;
 			hash(buffer, ss.str());
+			duration.start();
 			watch.start();
 			storage_.put(buffer, hash.getHashSize(), value, 200);
 			watch.stop();
+			duration.stop();
 		}
 		cout << "insert," << iter * ITEMS_PER_LOOPS + ITEMS_PER_LOOPS << ","
-				<< watch.getDuration() << endl;
+				<< "," << duration.getDuration() << watch.getDuration() << endl;
 		watch.reset();
 	}
+	duration.reset();
 	for (int iter = 0; iter < LOOP_ITERATIONS; iter++) {
 		unsigned char * valuepnt;
 		size_t valuesize;
@@ -44,26 +48,31 @@ void SpeedTest::run() {
 			stringstream ss;
 			ss << "test" << iter * ITEMS_PER_LOOPS + i;
 			hash(buffer, ss.str());
+			duration.start();
 			watch.start();
 			storage_.get(buffer, hash.getHashSize(), &valuepnt, &valuesize);
 			watch.stop();
+			duration.stop();
 			delete valuepnt;
 		}
-		cout << "get," << iter * ITEMS_PER_LOOPS + ITEMS_PER_LOOPS << "," << watch.getDuration()
-				<< endl;
+		cout << "get," << iter * ITEMS_PER_LOOPS + ITEMS_PER_LOOPS << ","
+				<< watch.getDuration() << "," << duration.getDuration() << endl;
 		watch.reset();
 	}
+	duration.reset();
 	for (int iter = 0; iter < LOOP_ITERATIONS; iter++) {
 		for (int i = 0; i < ITEMS_PER_LOOPS; i++) {
 			stringstream ss;
 			ss << "test" << iter * ITEMS_PER_LOOPS + i;
 			hash(buffer, ss.str());
+			duration.start();
 			watch.start();
 			storage_.del(buffer, hash.getHashSize());
 			watch.stop();
+			duration.stop();
 		}
-		cout << "del," << iter * ITEMS_PER_LOOPS + ITEMS_PER_LOOPS << "," << watch.getDuration()
-				<< endl;
+		cout << "del," << iter * ITEMS_PER_LOOPS + ITEMS_PER_LOOPS << ","
+				<< watch.getDuration() << "," << duration.getDuration() << endl;
 		watch.reset();
 	}
 }
