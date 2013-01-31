12468cb9771dff8040c15e9b644834493b809cb0
Till Lorentzen <t.lorentzen@tu-bs.de>
2013-01-31 01:13:09 +0100

    Changed plain c program example to minimum version
    The plain c set example puts 10000 elements into a
    set and uses only about 52 KByte memory consumption

--- setsync/main.c
+++ setsync/main.c
@@ -4,7 +4,7 @@
  *  Created on: 01.08.2012
  *      Author: Till Lorentzen
  */
-
+#define SET_SIZE 10000
 #include "set.h"
 
 #include <stdio.h>
@@ -14,29 +14,37 @@ int main(int argc, const char* argv[]) {
 	SET s;
 	SET_CONFIG c = set_create_config();
 	c.storage = BERKELEY_DB;
+	c.storage_cache_bytes = 20 * 1024;
 	result = set_init(&s, c);
 	if (result != 0) {
 		printf("error on init berkeley db set: %s\n",
 				set_last_error_to_string(&s));
 		return -1;
 	}
-	result = set_insert_string(&s, "test");
-	if (!result) {
-		printf("adding \"test\" failed\n");
-		set_free(&s);
-		return result;
+	unsigned int i;
+	size_t n;
+	char buffer[50];
+	for (i = 0; i < SET_SIZE; i++) {
+		n = sprintf(buffer, "test %d", i);
+		result = set_insert_string(&s, buffer);
+		if (!result) {
+			printf("adding %s failed\n",buffer);
+			set_free(&s);
+			return result;
+		}
 	}
-	result = set_find_string(&s, "test");
+/*	result = set_find_string(&s, "test");
 	if (!result) {
 		printf("finding \"test\" failed\n");
 		set_free(&s);
 		return result;
-	}
+	} */
 	result = set_free(&s);
 	if (result != 0) {
 		printf("error on deleting set1: %d\n", result);
 		return result;
 	}
+	/*
 	SET s2;
 	SET_CONFIG c2 = set_create_config();
 	c2.storage = LEVELDB;
@@ -59,13 +67,14 @@ int main(int argc, const char* argv[]) {
 		return result;
 	}
 	SET_SYNC_HANDLE handle;
-	set_sync_init_handle(&s2,&handle);
+	set_sync_init_handle(&s2, &handle);
 	printf("%ld\n", set_sync_calc_output_buffer_size(&handle, 1000, 1000));
 	set_sync_free_handle(&handle);
 	result = set_free(&s2);
 	if (result != 0) {
 		printf("error on deleting set2: %d\n", result);
-	}
+	}*/
 	return result;
+
 }
 
